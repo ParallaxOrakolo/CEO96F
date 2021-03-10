ae23162842280787e56c30a551ef3d0081f23320
Henrycke B.S <Oschenberk@gmail.com>
2021-03-10 16:55:27 -0300

    [Feature]: Find circles and search for screws recursively.
    Usando as funções "findHoles" e "findScrews", é possível obter de forma recursiva "n" distâncias.

--- ElementoTRAC.py
+++ ElementoTRAC.py
@@ -21,7 +21,7 @@ pFA = (50, 50)
 pFB = (100, 60)
 fixPoint = (pFB[0], int(pFA[1]+((pFB[1]-pFA[1])/2)))
 
-Read = 'P_ (2).jpg' if not Hole else 'A2.jpg'
+Read = 'P_ (3).jpg' if not Hole else 'A3.jpg'
 pathImages = 'Images/'
 
 markerOffset = 5
@@ -49,7 +49,7 @@ def empty(a):
     return a
 
 
-def finCircle(circle_Mask, areaMinC, areaMaxC, perimeter_size, blur_Size=3):
+def findCircle(circle_Mask, areaMinC, areaMaxC, perimeter_size, blur_Size=3):
     circle_info = []
     blur = cv2.medianBlur(circle_Mask, blur_Size)
     thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
@@ -67,6 +67,61 @@ def finCircle(circle_Mask, areaMinC, areaMaxC, perimeter_size, blur_Size=3):
     return circle_info
 
 
+def findScrew(imgAnalyse, tabs):
+    width = int(Image.shape[1])
+    height = int(Image.shape[0])
+    offset_screw = 0.00
+    edge_analyze = imgAnalyse[0:height, 0:int(width * bh)]
+    chr_k = imgAnalyse
+    for aba in range(len(tabs)):
+        for value in HSVjson[str(aba)]['Valores']:
+            globals()[value] = Op.extractHSValue(HSVjson[str(aba + 1)]['Valores'], value)
+        msk = Op.refineMask(Op.HSVMask(edge_analyze, lower, upper))
+        cv2.rectangle(msk, (0, 0), (msk.shape[1], msk.shape[0]), (0, 0, 0), thickness=20)
+        chr_k = cv2.bitwise_and(edge_analyze, edge_analyze, mask=msk)
+        edge, null = Op.findContoursPlus(msk, AreaMin_A=tabs[aba]['areaMin'], AreaMax_A=tabs[aba]['areaMax'])
+        if edge:
+            for info_edge in edge:
+                cv2.drawContours(chr_k, [info_edge['contour']], -1, (70, 255, 20), 3)
+                if aba == 0:
+                    point_a = tuple(info_edge['contour'][0])
+                    edge_analyze = imgAnalyse[point_a[1]: height, 0:width]
+                    pass
+                if aba == 1:
+                    offset_screw = round(info_edge['dimension'][0], 3)
+                    cv2.putText(chr_k, str(offset_screw), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (255, 255, 255), thickness=3)
+        else:
+            return offset_screw, chr_k
+    return offset_screw, chr_k
+
+
+def findHole(imgAnalyse, minArea, maxArea, c_perimeter):
+    for values in HSValues:
+        globals()[values] = np.array(Op.extractHSValue(HSValues, values))
+
+    mask = Op.refineMask(Op.HSVMask(imgAnalyse, lower, upper))
+    chr_k = cv2.bitwise_and(imgAnalyse, imgAnalyse, mask=mask)
+    distances = []
+    edge = findCircle(mask, minArea, maxArea, c_perimeter)
+    for Circle in edge:
+        cv2.circle(chr_k, (int(Circle['center'][0]), int(Circle['center'][1])), int(Circle['radius']),
+                   (36, 255, 12), 2)
+
+        cv2.line(chr_k, (int(Circle['center'][0]), int(Circle['center'][1])), fixPoint, (168, 50, 131))
+
+        cv2.line(chr_k, (int(Circle['center'][0]), int(Circle['center'][1])),
+                 (int(Circle['center'][0]), fixPoint[1]), (255, 0, 0))
+
+        cv2.line(chr_k, (int(Circle['center'][0]), fixPoint[1]), fixPoint, (0, 0, 255), thickness=2)
+
+        distance_to_fix = (round((Circle['center'][0] - fixPoint[0]), 3), round((Circle['center'][1] - fixPoint[1]), 3))
+        cv2.putText(chr_k, str(distance_to_fix[1]), (int(Circle['center'][0]), int(Circle['center'][1] / 2)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+        cv2.putText(chr_k, str(distance_to_fix[0]), (int(Circle['center'][0] / 2), int(fixPoint[1])),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+        distances.append([distance_to_fix])
+    return distances, chr_k
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
 #                                                    Json-File                                                         #
 
@@ -92,23 +147,8 @@ else:
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
 #                                                    Code-Exec                                                         #
-
-
-# Todo: Make a square around the cell.
-
-Image = Quadrants[line][column]
-
-for filterValues in HSValues:
-    locals()[filterValues] = Op.extractHSValue(HSValues, filterValues)
-
-for holeValue in HoleValues:
-    locals()[holeValue] = HoleValues[holeValue]
-
-largura2 = int(Image.shape[1])
-altura2 = int(Image.shape[0])
-edgeAnalyze = Image[0:altura2, 0:int(largura2 * bh)]
-
 # Todo: Make the config window with QT5.
+"""
 if DebugControls:
     cv2.namedWindow("Controls")
     cv2.resizeWindow("Controls", 600, 600)
@@ -186,62 +226,27 @@ if DebugControls:
             cv2.destroyAllWindows()
             print(lower, upper)
             break
-else:
+"""
 
-    # Todo: Transform in another file or function to use.
+# Todo: Make a square around the cell.
+Image = Quadrants[line][column]
 
-    Tabs = [mainConfig['Edge'], mainConfig['Screw']]
 
-    mask = Op.refineMask(Op.HSVMask(Image, lower, upper))
-    chroma_key = cv2.bitwise_and(Image, Image, mask=mask)
-    cv2.rectangle(chroma_key, pFA, pFB, (33, 48, 4), -1)
-    cv2.drawMarker(chroma_key, fixPoint, (0, 255, 0), markerSize=10, thickness=1)
+for filterValues in HSValues:
+    locals()[filterValues] = Op.extractHSValue(HSValues, filterValues)
 
-    if Hole:
-        for filterValues in HSValues:
-            locals()[filterValues] = np.array(Op.extractHSValue(HSValues, filterValues))
+for holeValue in HoleValues:
+    locals()[holeValue] = HoleValues[holeValue]
 
-        edgeInfo = finCircle(mask, areaMin, areaMax, perimeter)
-        for Circle in edgeInfo:
-            cv2.circle(chroma_key, (int(Circle['center'][0]), int(Circle['center'][1])), int(Circle['radius']),
-                       (36, 255, 12), 2)
+Tabs = [mainConfig['Edge'], mainConfig['Screw']]
+distance, chroma_key = findHole(Image, areaMin, areaMax, perimeter)
+cv2.imshow('Chroma Key', cv2.resize(chroma_key, None, fx=Escala, fy=Escala))
+cv2.imshow('Image', cv2.resize(Image, None, fx=Escala, fy=Escala))
+print(distance)
+cv2.waitKey(0)
 
-            cv2.line(chroma_key, (int(Circle['center'][0]), int(Circle['center'][1])), fixPoint, (168, 50, 131))
-
-            cv2.line(chroma_key, (int(Circle['center'][0]), int(Circle['center'][1])),
-                     (int(Circle['center'][0]), fixPoint[1]), (255, 0, 0))
-
-            cv2.line(chroma_key, (int(Circle['center'][0]), fixPoint[1]), fixPoint, (0, 0, 255), thickness=2)
-
-            distance = (round((Circle['center'][0]-fixPoint[0]), 3), round((Circle['center'][1] - fixPoint[1]), 3))
-            cv2.putText(chroma_key, str(distance[1]), (int(Circle['center'][0]), int(Circle['center'][1]/2)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
-            cv2.putText(chroma_key, str(distance[0]), (int(Circle['center'][0]/2), int(fixPoint[1])),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-    else:
-        for Tab in range(len(Tabs)):
-            for filterValues in HSVjson[str(Tab + 1)]['Valores']:
-                locals()[filterValues] = Op.extractHSValue(HSVjson[str(Tab + 1)]['Valores'], filterValues)
-            mask = Op.refineMask(Op.HSVMask(edgeAnalyze, lower, upper))
-            cv2.rectangle(mask, (0, 0), (mask.shape[1], mask.shape[0]), (0, 0, 0), thickness=20)
-            chroma_key = cv2.bitwise_and(edgeAnalyze, edgeAnalyze, mask=mask)
-            Contorno, Null = Op.findContoursPlus(mask, AreaMin_A=Tabs[Tab]['areaMin'], AreaMax_A=Tabs[Tab]['areaMax'])
-            for info in Contorno:
-                cv2.drawContours(chroma_key, [info['contour']], -1, (70, 20, 20), 3)
-                cv2.drawMarker(chroma_key, tuple(info['contour'][0]), (20, 20, 70), markerSize=10, thickness=3)
-                cv2.drawMarker(chroma_key, tuple(info['contour'][3]), (20, 20, 70), markerSize=10, thickness=3)
-                if Tab == 0:
-                    PL = tuple(info['contour'][0])
-                    PL1 = (int(largura2), int(info['contour'][0][1]))
-                    edgeAnalyze = Image[PL[1]: altura2, 0:largura2]
-                    pass
-                if Tab == 1:
-                    cv2.line(chroma_key, tuple(info['contour'][0]), tuple(info['contour'][1]),
-                             (0, 255, 255), thickness=3)
-                    distance = info['contour'][0][1] - info['contour'][1][1]
-                    cv2.putText(chroma_key, str(distance), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                                (255, 255, 255), thickness=3)
-    print(distance)
-    cv2.imshow('Chroma Key', cv2.resize(chroma_key, None, fx=Escala, fy=Escala))
-    cv2.imshow('Image', cv2.resize(Image, None, fx=Escala, fy=Escala))
-    cv2.waitKey(0)
+distance, chroma_key = findScrew(Image, Tabs)
+cv2.imshow('Chroma Key', cv2.resize(chroma_key, None, fx=Escala, fy=Escala))
+cv2.imshow('Image', cv2.resize(Image, None, fx=Escala, fy=Escala))
+print(distance)
+cv2.waitKey(0)
