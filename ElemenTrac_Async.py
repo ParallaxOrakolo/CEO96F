c6970ac8dad87a3e175b42dda74129bf0a6ab705
Henrycke B.S <Oschenberk@gmail.com>
2021-04-14 08:29:41 -0300

    Async Version Created

--- ElemenTrac_Async.py
+++ ElemenTrac_Async.py
@@ -1,11 +1,14 @@
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
 #                                                    Imports                                                           #
-import FastFunctions as Fast
+import FastFunctions as Ff
 import OpencvPlus as Op
 import operator as opr
 import numpy as np
+import asyncio
 import timeit
+from random import randint
 import cv2
+
 T0A = timeit.default_timer()
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
 #                                                    Variables                                                         #
@@ -21,7 +24,7 @@ Edge = True
 
 pFB = (100, 60)
 pFA = (50, 50)
-fixPoint = (pFB[0], int(pFA[1]+((pFB[1]-pFA[1])/2)))
+fixPoint = (pFB[0], int(pFA[1] + ((pFB[1] - pFA[1]) / 2)))
 
 Read = 'P_ (3).jpg' if not Hole else 'A3.jpg'
 pathImages = 'Images/'
@@ -41,7 +44,8 @@ altura = int(Image.shape[0])
 column = 1
 line = 1
 
-
+show_resize = 0.5
+show_time = 5
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
 #                                                    Functions                                                         #
 
@@ -51,7 +55,7 @@ def empty(a):
     return a
 
 
-def findCircle(circle_Mask, areaMinC, areaMaxC, perimeter_size, blur_Size=3):
+async def findCircle(circle_Mask, areaMinC, areaMaxC, perimeter_size, blur_Size=3):
     circle_info = []
     blur = cv2.medianBlur(circle_Mask, blur_Size)
     thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
@@ -66,11 +70,12 @@ def findCircle(circle_Mask, areaMinC, areaMaxC, perimeter_size, blur_Size=3):
         if len(approx) > perimeter_size and areaMinC < area < areaMaxC:
             ((x, y), r) = cv2.minEnclosingCircle(c)
             circle_info.append({"center": (x, y), "radius": r})
+    await asyncio.sleep(0.0)
     return circle_info
 
 
-def findScrew(imgAnalyse, tabs):
-
+async def findScrew(event2, event3, show, imgAnalyse, tabs):
+    await asyncio.sleep(0.0)
     if type(imgAnalyse) != np.ndarray:
         print(type(imgAnalyse))
         imgAnalyse = Op.takeSnapshot(imgAnalyse)
@@ -99,12 +104,24 @@ def findScrew(imgAnalyse, tabs):
                     cv2.putText(chr_k, str(offset_screw), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1,
                                 (255, 255, 255), thickness=3)
         else:
+            if show:
+                asyncio.create_task(ShowImage('Screw', chr_k, show_resize, show_time, asyncio.Event()))
+            else:
+                event2.set()
             return offset_screw, chr_k
+    if show:
+        asyncio.create_task(ShowImage('Screw', chr_k, show_resize, show_time, asyncio.Event()))
+    else:
+        event2.set()
+
+    # while not event3.is_set():
+    #     pass
     return offset_screw, chr_k
 
 
-def findHole(imgAnalyse, minArea, maxArea, c_perimeter):
-
+async def findHole(event2, event3, show, imgAnalyse, minArea, maxArea, c_perimeter):
+    await asyncio.sleep(0.0)
+    global img
     if type(imgAnalyse) != np.ndarray:
         print(type(imgAnalyse))
         imgAnalyse = Op.takeSnapshot(imgAnalyse)
@@ -115,7 +132,7 @@ def findHole(imgAnalyse, minArea, maxArea, c_perimeter):
     mask = Op.refineMask(Op.HSVMask(imgAnalyse, lower, upper))
     chr_k = cv2.bitwise_and(imgAnalyse, imgAnalyse, mask=mask)
     distances = []
-    edge = findCircle(mask, minArea, maxArea, c_perimeter)
+    edge = await asyncio.create_task(findCircle(mask, minArea, maxArea, c_perimeter))
     for Circle in edge:
         cv2.circle(chr_k, (int(Circle['center'][0]), int(Circle['center'][1])), int(Circle['radius']),
                    (36, 255, 12), 2)
@@ -132,14 +149,63 @@ def findHole(imgAnalyse, minArea, maxArea, c_perimeter):
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
         cv2.putText(chr_k, str(distance_to_fix[0]), (int(Circle['center'][0] / 2), int(fixPoint[1])),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-        distances.append([distance_to_fix])
+        distances.append([distance_to_fix, 'id:', randint(250, 500)])
+    if show:
+        asyncio.create_task(ShowImage('Hole', chr_k, show_resize, show_time, event2))
+    else:
+        event2.set()
+
+    while not event3.is_set():
+        print("Preso")
+        await asyncio.sleep(0.3)
+    print("Soltou")
     return distances, chr_k
+
+
+async def CloseProcess(event, event2):
+    while not event.is_set():
+        if event2.is_set():
+            event.set()
+        await asyncio.sleep(0.0)
+
+
+async def ShowImage(name, img, resize, time, close_event, debug=False, permanent=False):
+    close_event.clear()
+    if debug:
+        print("ShowImage")
+    if permanent:
+        while not close_event.is_set():
+            cv2.imshow(str(name), cv2.resize(img, None, fx=resize, fy=resize))
+            await asyncio.sleep(0.0)
+    else:
+        # cv2.imshow(str(name), cv2.resize(img, None, fx=resize, fy=resize))
+        cv2.imshow(str(name), cv2.resize(img, None, fx=resize, fy=resize))
+        cv2.waitKey(1)
+        await asyncio.sleep(time)
+        close_event.set()
+
+
+async def Main():
+    event = asyncio.Event()
+    event2 = asyncio.Event()
+    event3 = asyncio.Event()
+    nada = asyncio.create_task(CloseProcess(event, event2))
+    hct = timeit.default_timer()
+    RESERVA_resultado = asyncio.gather(*[findHole(event2, event3, True, Image, areaMin, areaMax, perimeter),
+                                         findScrew(event2, event3, True, Image, Tabs)])
+    print("Criação a Thread", round(timeit.default_timer()-hct, 5))
+    event3.set()
+    hcd = timeit.default_timer()
+    resultado = await RESERVA_resultado
+    if resultado:
+        print("Validação de dados:", round(timeit.default_timer()-hcd, 5))
+    nada = await nada
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
 #                                                    Json-File                                                         #
 
 
 HSVjsonIndex = 0
-mainConfig = Fast.readJson('Json/config.json')
+mainConfig = Ff.readJson('Json/config.json')
 HSVjson = mainConfig['Filtros']['HSV']
 HSValues = HSVjson[str(HSVjsonIndex)]['Valores']
 
@@ -156,22 +222,19 @@ else:
     DebugMarkings = DebugTypes["markings"]
     DebugControls = DebugTypes["testControls"]
 
-
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
-#                                                    Code-Exec                                                         #
+#                                              Variables - with json                                                   #
 
-arduino = Fast.SerialConnect(SerialPath='Json/serial.json', name='Ramps 1.4')
+arduino = Ff.SerialConnect(SerialPath='Json/serial.json', name='Ramps 1.4')
 
 # Make a square around the cell.
 p1 = tuple(map(opr.add, map(opr.mul, (((Quadrants[line][column]).shape[:2])[::-1]), (column, line)), (-10, -10)))
-p2 = tuple(map(opr.add, map(opr.mul, (((Quadrants[line-1][column-1]).shape[:2])[::-1]), (column+1, line+1)), (10, 10)))
+p2 = tuple(
+    map(opr.add, map(opr.mul, (((Quadrants[line - 1][column - 1]).shape[:2])[::-1]), (column + 1, line + 1)), (10, 10)))
 cv2.rectangle(Image, p1, p2, (0, 0, 255), 5)
-
-# cv2.imshow('Image_Original', cv2.resize(Image, None, fx=Escala*0.3, fy=Escala*0.3))
-
-
+cv2.imshow('Image_Original', cv2.resize(Image, None, fx=Escala * 0.35, fy=Escala * 0.35))
+cv2.waitKey(0)
 Image = Quadrants[line][column]
-
 
 for filterValues in HSValues:
     locals()[filterValues] = Op.extractHSValue(HSValues, filterValues)
@@ -181,6 +244,14 @@ for holeValue in HoleValues:
 
 Tabs = [mainConfig['Mask_Parameters']['Edge'], mainConfig['Mask_Parameters']['Screw']]
 
-T0A = timeit.default_timer()
-Resultados = [findHole(Image, areaMin, areaMax, perimeter), findScrew(Image, Tabs)]
-print("Tempo de Execução:", round(timeit.default_timer()-T0A, 3))
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= #
+#                                                    Code-Exec                                                         #
+
+asyncio.run(Main())
+
+print("Tempo de Execução:", round(timeit.default_timer() - T0A, 3))
+
+
+
+
