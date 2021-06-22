34042ca741f7b186dd0e79558170f3e8c607d600
Henrycke B.S <Oschenberk@gmail.com>
2021-06-22 16:46:23 -0300

    Add Rois

--- engine_H/OpencvPlus.py
+++ engine_H/OpencvPlus.py
@@ -1,6 +1,7 @@
-import cv2
+import FastFunctions as Fast
 import numpy as np
 import math
+import cv2
 
 
 # Organiza todos os valores de uma matriz bidimensional em ordem crescente.
@@ -93,6 +94,60 @@ def findContoursPlus(image, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE, 
         return outPut_A, outPut_B
     except TypeError:
         return [], []
+
+
+def ControlWindow(H=(0, 255), S=(0, 255), V=(0, 255), A=(5000, 3000)):
+    def empty(a):
+        pass
+    cv2.namedWindow("Controle")
+    cv2.resizeWindow("Controle", 600, 600)
+    cv2.createTrackbar("H Min", "Controle", H[0], 255, empty)
+    cv2.createTrackbar("H Max", "Controle", H[1], 255, empty)
+    cv2.createTrackbar("S Min", "Controle", S[0], 255, empty)
+    cv2.createTrackbar("S Max", "Controle", S[1], 255, empty)
+    cv2.createTrackbar("V Min", "Controle", V[0], 255, empty)
+    cv2.createTrackbar("V Max", "Controle", V[1], 255, empty)
+    cv2.createTrackbar("A Min", "Controle", A[0], 15000, empty)
+    cv2.createTrackbar("A Max", "Controle", A[1], 80000, empty)
+    cv2.createTrackbar("p", "Controle", 0, 10, empty)
+
+
+def GetControlWindow():
+    h_min = cv2.getTrackbarPos("H Min", "Controle")
+    s_min = cv2.getTrackbarPos("S Min", "Controle")
+    v_min = cv2.getTrackbarPos("V Min", "Controle")
+
+    h_max = cv2.getTrackbarPos("H Max", "Controle")
+    s_max = cv2.getTrackbarPos("S Max", "Controle")
+    v_max = cv2.getTrackbarPos("V Max", "Controle")
+
+    Amin = cv2.getTrackbarPos("A Min", "Controle")
+    Amax = cv2.getTrackbarPos("A Max", "Controle")
+    return {"lower": np.array([h_min, s_min, v_min]),
+            "upper": np.array([h_max, s_max, v_max]),
+            "a_min": Amin, "a_max": Amax}
+
+
+def Rois(img, n=4, escale=0.25, Save=True, show=False):
+    output = []
+    Pos=[]
+    for _ in range(n):
+        A = list(cv2.selectROI(f"Roi_{_}", cv2.resize(img, None, fx=escale, fy=escale)))
+        for aa in range(len(A)):
+            A[aa] = int(A[aa]/escale)
+
+        frame = img[A[1]:A[1] + A[3], A[0]:A[0] + A[2]]
+        if show:
+            cv2.destroyAllWindows()
+            cv2.imshow("Frames", frame)
+            cv2.waitKey(500)
+
+        output.append(frame)
+        Pos.append({"P0": tuple([A[0], A[1]]), "P1": tuple([A[2], A[3]])})
+    if Save:
+        Fast.writeJson('Cuts.json', Pos)
+
+    return output
 
 
 # Cria uma mascara HSV apartir de um range pré-determinado.
