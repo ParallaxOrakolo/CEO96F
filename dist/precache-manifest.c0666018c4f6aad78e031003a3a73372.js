ed04bee63d3cf3402e90f7389c86d6c6cce9d6ff
Henrycke <oschenberk@gmail.com>
2021-08-02 08:34:19 -0300

    Update from rasp

---
+++ dist/precache-manifest.c0666018c4f6aad78e031003a3a73372.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b84d3001c54269ad5d2",
+    "url": "/css/app.bae61e56.css"
+  },
+  {
+    "revision": "31b0bebdbbdc02fba483",
+    "url": "/css/chunk-vendors.adeef397.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "bf61930d5682ec06a440e72e18cb0eec",
+    "url": "/img/estribo-pattern.bf61930d.jpg"
+  },
+  {
+    "revision": "12190c35437db902601fd100dd6fac18",
+    "url": "/img/parallax-logo-06.12190c35.svg"
+  },
+  {
+    "revision": "78fd50eec3a7b4e2e04abe1bdf2af475",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b84d3001c54269ad5d2",
+    "url": "/js/app.3b4bcf3c.js"
+  },
+  {
+    "revision": "31b0bebdbbdc02fba483",
+    "url": "/js/chunk-vendors.4e324dbd.js"
+  },
+  {
+    "revision": "7cfe264e7f1084d3fe8ef2cec97ff0c6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "0b65a7779a072080aa3377599d4ed0be",
+    "url": "/media/estribo-animation.0b65a777.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
