5eeca98918ff413e20adb8e5c22d7ffec3d9ba04
Henrycke <oschenberk@gmail.com>
2021-08-12 17:06:07 -0300

    new build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36a37dd59dbfe07edc97c2c3313e8f8f.js"
+  "/precache-manifest.4a083d17a4d49c5455869fef15fec50f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "interfacePnp"});
