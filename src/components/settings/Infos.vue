8bba76854a5a4ad379ef5b7c3b395d52f597f0d7
Rodrigo Gomes <rodrigogomes51@gmail.com>
2021-10-05 11:22:51 -0300

    Clean Code
    update do .gitgnore

--- src/components/settings/Infos.vue
+++ src/components/settings/Infos.vue
@@ -1,13 +1,13 @@
 <template>
   <v-expansion-panel>
     <v-expansion-panel-header>
-      <div><v-icon class="mr-2">mdi-information-outline</v-icon>
-      Informações</div>
-      </v-expansion-panel-header
-    >
+      <div>
+        <v-icon class="mr-2">mdi-information-outline</v-icon> Informações
+      </div>
+    </v-expansion-panel-header>
     <v-expansion-panel-content>
       <v-divider></v-divider>
-     
+
       <v-list three-line subheader>
         <v-list-item>
           <v-list-item-content>
@@ -28,8 +28,10 @@
           <v-list-item-content>
             <v-list-item-title>Versões</v-list-item-title>
             <v-list-item-subtitle
-              ><b>Frontend </b
-              >{{ configuration.informations.version.frontend }} |
+              ><b>Frontend </b>
+              <!-- {{ configuration.informations.version.frontend }}  -->
+              {{ getFrontVersion() }}
+              |
               <b>Backend</b> {{ configuration.informations.version.backend }} |
               <b>Marlin</b> {{ configuration.informations.version.marlin }}
             </v-list-item-subtitle>
@@ -52,6 +54,12 @@ export default {
   }),
 
   methods: {
+    getFrontVersion() {
+      var pjson = require("../../../package.json");
+      console.log("font version: ", pjson.version);
+      return pjson.version;
+    },
+
     IsVisible() {
       this.snackbar = true;
     },
